fd45f95a7f83ff3a9f7ecb594591246406a01757
Gianni <gianni-ruffini@outlook.fr>
2022-08-15 16:44:35 +0100

    mise a jour des menus

--- src/Entity/ContenusAccueil.php
+++ src/Entity/ContenusAccueil.php
@@ -26,12 +26,12 @@ class ContenusAccueil
     private $titre;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=255, nullable=false)
      */
     private $sousTitre;
 
     /**
-     * @ORM\Column(type="text", nullable=true)
+     * @ORM\Column(type="text", nullable=false)
      */
     private $description;
 
